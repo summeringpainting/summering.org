094c6681aefc01c099762a7bd4967e9fb06d47b5
summeringpainting <summeringpainting@gmail.com>
2022-08-26 19:17:44 -0400

    go go audio stream

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import requests
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route("/radio")
+def radio():
+    r = requests.get("http://localhost:8000/radio.ogg", stream=True)
+    return requests.Response((r.iter_content(chunk_size=1024), mimetype="audio/ogg"))
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
