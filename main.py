89d14078ba873a506950b6659f42af61de91003e
summeringpainting <summeringpainting@gmail.com>
2022-08-26 21:26:33 -0400

    ogg to mp3

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
+
 @app.route("/audio_stream")
 def Audio_Stream():
-     r = requests.get("http://localhost:8000/radio.ogg", stream=True)
-     return Response(r.iter_content(chunk_size=1024), mimetype='audio/mpeg')
+     r = requests.get("http://localhost:8000/radio.mp3", stream=True)
+     return Response(r.iter_content(chunk_size=1024), mimetype='audio/mp3')
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=5000)
