fbd45d18e8c8b0f174a2b3b685e9392c9c2b1c3c
summeringpainting <summeringpainting@gmail.com>
2022-08-26 22:54:15 -0400

    lo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/audio_stream")
+@app.route("/audio_stream.mp3")
 def Audio_Stream():
      r = requests.get("http://localhost:8000/radio.mp3", stream=True)
      return Response(r.iter_content(chunk_size=1024), mimetype='audio/mp3')
