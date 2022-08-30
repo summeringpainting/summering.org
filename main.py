934ad6f80f9f7adca291815ddaec6fe78a27eb38
Dexter S <dexter@pop-os.localdomain>
2022-08-29 23:33:07 -0400

    hi

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
+from turtle import dot
 from flask import Flask, render_template, Response
 import requests
+import os
+import eyed3
+from dotenv import load_dotenv, find_dotenv
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+load_dotenv(find_dotenv())
+print(os.getenv("STATS_DOMAIN"))
+
 
 @app.route('/')
 def home():
-    statsurl = "http://10.0.0.125:8000/status.xsl"
+    statsurl = os.getenv("STATS_DOMAIN")
     s = requests.get(statsurl).text
     soup = BeautifulSoup(s, 'html.parser')
     stats = []
+    md = {}
     for row in soup.find_all('tr'):
         stats.append(row.get_text())
-    return render_template("index.html", stats=stats)
+    #file = stats[8].split(" - ")[1] + ".mp3"
+    #tag = eyed3.load(os.getenv("MUSIC_DIR") + file)
+    # print(tag.tag.album)
+    return render_template("index.html", stats=stats, md=md)
 
 
 @app.route("/audio_stream.mp3")
 def Audio_Stream():
-    streamurl = "http://10.0.0.125:8000/radio.mp3"
+    streamurl = os.getenv("RADIO_DOMAIN")
     headers = {'Accept-Ranges': 'bytes'}
     r = requests.get(streamurl, headers=headers, stream=True)
-    song = Response(r.iter_content(chunk_size=1024), mimetype='audio/mp3', direct_passthrough=True)
+    song = Response(r.iter_content(chunk_size=1024),
+                    mimetype='audio/mp3', direct_passthrough=True)
     return song
-
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=5000)
-
